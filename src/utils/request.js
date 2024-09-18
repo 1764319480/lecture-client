e0e0c09b258b71394f08c7a7ae33894be9a6a166
ShiZhang <1764319480@qq.com>
2024-09-18 16:48:03 +0800

    add interceptor for request

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,5 +5,14 @@ const instance = axios.create({
     timeout: 2000
     
 })
-
+instance.interceptors.request.use(function (config) {
+    // console.log(config)
+    const token = sessionStorage.getItem('token') || sessionStorage.getItem('supertoken'); 
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
 export default instance
